bc293762ddbd61d9f787129e11181fe00e12be9b
Nihar Chopade <65650226+Bobya9595@users.noreply.github.com>
2026-03-12 13:14:44 +0530

    Create page.tsx

---
+++ app/experience-letter/page.tsx
@@ -0,0 +1,11 @@
+export default function ExperienceLetter() {
+  return (
+    <main className="min-h-screen bg-[#020617] text-white flex items-center justify-center">
+
+      <h1 className="text-4xl font-bold">
+        Experience Letter Generator
+      </h1>
+
+    </main>
+  );
+}
