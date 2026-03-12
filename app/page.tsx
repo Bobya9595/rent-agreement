0be54a18206a96f200929ee65a75e99af5bc32a7
Nihar Chopade <65650226+Bobya9595@users.noreply.github.com>
2026-03-12 12:39:46 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,14 +3,12 @@ import Navbar from "../components/Navbar";
 export default function Home() {
   return (
     <main className="min-h-screen bg-[#020617] text-white">
-
       <Navbar />
 
       <section className="flex flex-col items-center justify-center text-center py-32 px-6">
-
-        <h1 className="text-5xl font-bold leading-tight">
+        <h1 className="text-5xl font-bold">
           AI Legal Intelligence
-          <span className="block bg-gradient-to-r from-purple-500 to-blue-400 text-transparent bg-clip-text">
+          <span className="block text-purple-500">
             for Indian Agreements
           </span>
         </h1>
@@ -18,9 +16,7 @@ export default function Home() {
         <p className="text-gray-400 mt-6 max-w-xl">
           Generate rent agreements, affidavits, contracts and more in minutes using AI-powered automation.
         </p>
-
       </section>
-
     </main>
   );
 }
