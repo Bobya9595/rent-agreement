025bf1561c765ef37338d696c7ed33206f044231
Nihar Chopade <65650226+Bobya9595@users.noreply.github.com>
2026-03-27 15:32:25 +0530

    Create route.ts

---
+++ app/api/verify-payment/route.ts
@@ -0,0 +1,21 @@
+import { NextResponse } from "next/server";
+import crypto from "crypto";
+
+export async function POST(req: Request) {
+  const body = await req.json();
+
+  const { razorpay_order_id, razorpay_payment_id, razorpay_signature } = body;
+
+  const sign = razorpay_order_id + "|" + razorpay_payment_id;
+
+  const expectedSign = crypto
+    .createHmac("sha256", process.env.RAZORPAY_KEY_SECRET!)
+    .update(sign.toString())
+    .digest("hex");
+
+  if (expectedSign === razorpay_signature) {
+    return NextResponse.json({ success: true });
+  } else {
+    return NextResponse.json({ success: false }, { status: 400 });
+  }
+}
